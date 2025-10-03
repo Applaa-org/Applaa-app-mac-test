3785001ff6ae2ff950e2044e4065a5185559fab0
rahulbachina <rahulbachinas@gmail.com>
2025-10-03 20:29:41 +0100

    chore: temporarily disable WordPress authentication

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,13 +1,14 @@
 import { createRootRoute, Outlet } from "@tanstack/react-router";
 import Layout from "../app/layout";
-import { AuthGuard } from "../components/auth/AuthGuard";
+// import { AuthGuard } from "../components/auth/AuthGuard"; // DISABLED: WordPress auth temporarily disabled
 
 export const rootRoute = createRootRoute({
   component: () => (
-    <AuthGuard>
+    // DISABLED: WordPress authentication temporarily disabled
+    // <AuthGuard>
       <Layout>
         <Outlet />
       </Layout>
-    </AuthGuard>
+    // </AuthGuard>
   ),
 });
